a61604c470bdd8a3eabe0c2a994b9e7a80998d99
liuxvanchen <149084116+liuxvanchen@users.noreply.github.com>
2024-03-28 20:40:44 +0800

    dev

--- Pytorch/nn_conv2d.py
+++ Pytorch/nn_conv2d.py
@@ -28,20 +28,21 @@ print(juanji)
 # 表示 conv1 是一个二维卷积层 (Conv2d)，它的输入通道数为 3，输出通道数为 6，
 # 卷积核的大小为 3x3，卷积操作的步长为 (1, 1)。
 
-writer=SummaryWriter("conv2d_log")
+#writer=SummaryWriter("conv2d_log")
 step=0
 for data in dataloader:
     imgs,targets=data
     output=juanji(imgs)
     #print(output.shape)
-    print(imgs.shape)#torch.Size([64, 3, 32, 32])
+    #print(imgs.shape)#torch.Size([64, 3, 32, 32])
     print(output.shape)#torch.Size([64, 6, 30, 30])通道数量变多，图片变小
-    writer.add_images("input",imgs,step)
+    #writer.add_images("input",imgs,step)
 
     #将output进行一个reshape，-1指的是不知道多少但是自动生成
     output=torch.reshape(output,(-1,3,30,30))
-    writer.add_images("output",output,step)
+    print(output.shape)
+    #writer.add_images("output",output,step)
 
     step=step+1
 
-    writer.close()
+    #writer.close()
